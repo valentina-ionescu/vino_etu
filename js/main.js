b06898904ebc1e857835df5e417e980d38825e17
Djaouida Kharchi <djaouida.kharchi@gmail.com>
2021-10-27 20:11:02 -0400

    enleve notes de ajouternouvellebouteille

--- js/main.js
+++ js/main.js
@@ -202,11 +202,7 @@ window.addEventListener('load', function() {
             "vino__bouteille_id":bouteille.nom.dataset.id,
             "date_achat":bouteille.date_achat.value,
             "garde_jusqua":bouteille.garde_jusqua.value,
-<<<<<<< HEAD
             // "notes":bouteille.notes.value,
-=======
-           /* "notes":bouteille.notes.value,*/
->>>>>>> 1b01f345f54bc611630986bf0936bb73fb42385f
             "prix":bouteille.prix.value,
             "quantite":bouteille.quantite.value,
             "millesime":bouteille.millesime.value,
