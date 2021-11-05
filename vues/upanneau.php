88c0a9240528c16b6f68a5c20fb94e5832e62344
djaouida kharchi <djaouidakharchi@djaouidas-MacBook-Air.local>
2021-11-04 21:46:29 -0400

    ajout cellier

--- vues/upanneau.php
+++ vues/upanneau.php
@@ -12,11 +12,14 @@
 
 <h3 class="u__titre">Gestion des Celliers</h3>
 <p>Mes celliers</p>
-<div class="u__celliers_liste flex">
+<div class="u__celliers_liste flex col">
 <?php foreach ($dataC as $cle => $cel) { ?>
     <article class="u__article flex col">
        <button class="selectCellier" data-cellid="<?php echo $cel['id'] ?>"><?php echo $cel['nom_cellier'] ?></button> 
     </article> <?php }?>
-    
+    <form method="POST" action="index.php?requete=ajouterCellier">
+        <button class="u__ajout">Ajouter</button>
+    </form>
+ 
 </div>
 </section>
