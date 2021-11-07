0cdaf292f4e7e773c7224b4f1b7097e58bb8846f
Djaouida <djaouidakharchi@djaouidas-MacBook-Air.local>
2021-11-06 22:58:20 -0400

    css style panneau usager

--- vues/upanneau.php
+++ vues/upanneau.php
@@ -1,26 +1,21 @@
 <section class="u__profile__wrapper">
-<div class="u__menu">
-   
-  
-    <!-- <form method="POST" action="index.php?requete=profileConnexion"> -->
-    <!-- <button name="status" value="deconnexion">Deconnexion</button> -->
-    <!-- </form> -->
-
-    
-</div>
+    <div class="u__entete flex col">
+        <div class="overlay flex">
+            <h3 class="u__titre">Gestion des Celliers</h3>
+        </div>
+    </div>
 
 
-<h3 class="u__titre">Gestion des Celliers</h3>
-<p>Mes celliers</p>
-<div class="u__celliers_liste">
-<?php foreach ($dataC as $cle => $cel) { ?>
-    <article class="u__article">
-       <!-- <button class="selectCellier" data-cellid="<?php echo $cel['id'] ?>"><?php echo $cel['nom_cellier'] ?></button>  -->
-       <a href="" class="selectCellier" data-cellid="<?php echo $cel['id'] ?>"><?php echo $cel['nom_cellier'] ?></a><i class="far fa-trash-alt"></i> 
-    </article> <?php }?>
-    <form method="POST" action="index.php?requete=ajouterCellier">
-        <button class="u__ajout">Ajouter</button>
-    </form>
- 
-</div>
+    <div class="u__celliers_liste u__contenu">
+        <p>Mes celliers</p>
+        <?php foreach ($dataC as $cle => $cel) { ?>
+            <article class="u__article">
+                <!-- <button class="selectCellier" data-cellid="<?php echo $cel['id'] ?>"><?php echo $cel['nom_cellier'] ?></button>  -->
+                <a href="" class="selectCellier" data-cellid="<?php echo $cel['id'] ?>"><?php echo $cel['nom_cellier'] ?></a><i class="far fa-trash-alt"></i>
+            </article> <?php } ?>
+        <form method="POST" action="index.php?requete=ajouterCellier">
+            <button class="u__ajout">Ajouter</button>
+        </form>
+
+    </div>
 </section>
